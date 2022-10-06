abd31952bd12c8804bb562fc0905f9348ae4fe2e
Brenda Martins <brendamichele16@hotmail.com>
2022-10-06 15:19:53 -0300

    Improving exceptions

--- domain/src/main/java/com/admin/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/admin/catalog/domain/exceptions/DomainException.java
@@ -4,17 +4,21 @@ import com.admin.catalog.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException{
+public class DomainException extends NoStackTraceException{
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErros){
-        super("", null, true, false);
+    private DomainException(final String aMessage, final List<Error> anErros){
+        super(aMessage);
         this.errors = anErros;
     }
 
+    public static DomainException with(final Error anErros){
+        return new DomainException(anErros.message(), List.of(anErros));
+    }
+
     public static DomainException with(final List<Error> anErros){
-        return new DomainException(anErros);
+        return new DomainException("", anErros);
     }
 
     public List<Error> getErrors() {
