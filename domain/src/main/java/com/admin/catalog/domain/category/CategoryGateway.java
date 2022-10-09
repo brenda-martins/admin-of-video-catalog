ad5ac93603d6a4da72fb5921e0abe4aa119cfb9a
Brenda Martins <brendamichele16@hotmail.com>
2022-10-09 06:50:31 -0300

    Created interface Gateway for category

---
+++ domain/src/main/java/com/admin/catalog/domain/category/CategoryGateway.java
@@ -0,0 +1,20 @@
+package com.admin.catalog.domain.category;
+
+import com.admin.catalog.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    Category deleteById(CategoryID anId);
+
+    Optional<Category> findById(CategoryID anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+
+
+}
