af414f09b90a5227792a97029544311737751e06
Brenda Martins <brendamichele16@hotmail.com>
2022-10-24 13:53:54 -0300

    Implemented the category deletion use case

---
+++ application/src/main/java/com/admin/catalog/application/category/delete/DefaultDeleteCategoryUseCase.java
@@ -0,0 +1,20 @@
+package com.admin.catalog.application.category.delete;
+
+import com.admin.catalog.domain.category.CategoryGateway;
+import com.admin.catalog.domain.category.CategoryID;
+
+import java.util.Objects;
+
+public class DefaultDeleteCategoryUseCase extends DeleteCategoryUseCase{
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultDeleteCategoryUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public void execute(final String anId) {
+        this.categoryGateway.deleteById(CategoryID.from(anId));
+    }
+}
